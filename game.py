aecb7679b26a5716f111399feac2d9a83480f066
Nick Cardoza <cardoza.nick21@gmail.com>
2021-07-20 18:28:48 -0500

    added function to show score

--- game.py
+++ game.py
@@ -13,5 +13,9 @@ class Game:
         self.snake = Snake(self)
 
         self.window = pygame.display.set_mode((self.game_width, self.game_height))
+        self.score = 0
         # self.bg = pygame.transform.scale(pygame.image.load("assets/background.png"),(self.game_width, self.game_height))
         # self.bg = self.window.fill(BLACK)
+
+    def show_score(self, font, color):
+        self.window.blit(font.render(f"Score: {str(self.score)}", 1, color), (10, 20))
--- main.py
+++ main.py
@@ -1,41 +1,49 @@
 import pygame
 from game import Game
 
-FPS = 5
-BLACK = [0, 0, 0]
-
-
 pygame.init()
-clock = pygame.time.Clock()
+pygame.font.init()
+FPS = 8
+BLACK = [0, 0, 0]
+WHITE = [255, 255, 255]
+FONT = pygame.font.SysFont('arial', 20)
+
+def game_loop():
+    clock = pygame.time.Clock()
 
 
-game = Game(600, 600)
-snake = game.snake
-food = game.food
+    game = Game(600, 600)
+    snake = game.snake
+    food = game.food
 
-playing = True
+    playing = True
 
-# main game loop
-while playing:
-    clock.tick(FPS)
-    events = pygame.event.get()
+    # main game loop
+    while playing:
+        clock.tick(FPS)
+        # clock.get_time()
+        events = pygame.event.get()
 
-    # check if 'x' is pressed
-    for event in events:
-        if event.type == pygame.QUIT:
-            pygame.quit()
+        # check if 'x' is pressed
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
 
-    # game.window.blit(game.bg, (0,0))
-    game.window.fill(BLACK)
+        # game.window.blit(game.bg, (0,0))
+        game.window.fill(BLACK)
 
-    food.show_food(game, food.x, food.y)
-    if food.eat_food(game, snake):
-        snake.grow(game)
-    events = snake.move(events)
-    playing = snake.is_dead(game)
-    snake.update(game)
-    
-    pygame.display.update()
+        food.show_food(game, food.x, food.y)
+        if food.eat_food(game, snake):
+            snake.grow(game)
+        events = snake.move(events)
+        playing = snake.is_dead(game)
+        snake.update(game)
+        game.show_score(FONT, WHITE)
+        
+        pygame.display.update()
+    game_loop()
+
+game_loop()
 
 
 
